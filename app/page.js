430704696d3c21c6962e36deff90b436111b3693
AlejandroPose <alepose.ve@gmail.com>
2023-09-15 03:11:04 +0200

    locuron

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Empezando a editar trabajar en este archivo&nbsp;
+          Empezando a editar en este archivo&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
